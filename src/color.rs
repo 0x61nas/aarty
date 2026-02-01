7500a691f51126bf24fc2c37ea6491520de4599b
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2026-02-01 13:33:40 +0200

    refactor(color): drop the string use

--- src/color.rs
+++ src/color.rs
@@ -7,28 +7,33 @@ pub(crate) const ANSI_FOREGROUND_ESCAPE: &str = "\u{001b}[38;2;";
 pub(crate) const ANSI_BACKGROUND_ESCAPE: &str = "\u{001b}[48;2;";
 pub(crate) const ANSI_COLOR_CODE_LEN: usize = 12;
 pub(crate) const TRANSPARENT: ANSIColor = ANSIColor {
-    inner: String::new(),
+    inner: [0u8; ANSI_COLOR_CODE_LEN],
 };
 
 /// ansi color.
 #[derive(Debug, PartialEq, PartialOrd, Clone, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct ANSIColor {
-    inner: String,
+    inner: [u8; ANSI_COLOR_CODE_LEN],
 }
 
 impl ANSIColor {
     /// Construct a new instance.
     pub fn new(r: u8, g: u8, b: u8) -> ANSIColor {
-        let mut c = String::with_capacity(ANSI_COLOR_CODE_LEN);
+        let mut c = [0u8; ANSI_COLOR_CODE_LEN];
 
+        let mut cur = 0usize;
         for i in [r, g, b] {
-            c.push_str(&i.to_string());
-            c.push(';');
+            for ch in i.to_string().chars() {
+                c[cur] = ch as u8;
+                cur += 1;
+            }
+            c[cur] = b';';
+            cur += 1;
         }
-        let _ = c.pop();
+        c[cur - 1] = 0;
 
-        debug_assert_eq!(c.capacity(), ANSI_COLOR_CODE_LEN);
+        debug_assert_eq!(c.len(), ANSI_COLOR_CODE_LEN);
 
         ANSIColor { inner: c }
     }
@@ -36,18 +41,43 @@ impl ANSIColor {
     /// return true if the color is transparent.
     #[inline(always)]
     pub fn is_transparent(&self) -> bool {
-        self.inner.is_empty()
+        self.inner[0] == 0
     }
 
     /// Return an ANSI escaped background color.
     pub fn as_background(&self) -> String {
-        format!("{ANSI_BACKGROUND_ESCAPE}{}m", self.inner)
+        let len = self.calc_inner_len();
+        unsafe {
+            format!(
+                "{ANSI_BACKGROUND_ESCAPE}{}m",
+                std::str::from_utf8_unchecked(&self.inner[..len])
+            )
+        }
     }
 
     /// Return an ANSI escaped foreground color.
     pub fn as_foreground(&self) -> String {
-        format!("{ANSI_FOREGROUND_ESCAPE}{}m", self.inner)
+        let len = self.calc_inner_len();
+        unsafe {
+            format!(
+                "{ANSI_FOREGROUND_ESCAPE}{}m",
+                std::str::from_utf8_unchecked(&self.inner[..len])
+            )
+        }
     }
+
+    #[inline]
+    fn calc_inner_len(&self) -> usize {
+        let mut len = self.inner.len();
+        for (i, c) in self.inner.into_iter().enumerate() {
+            if c == 0 {
+                len = i;
+                break;
+            }
+        }
+        len
+    }
+
 }
 
 impl Display for ANSIColor {
@@ -56,10 +86,15 @@ impl Display for ANSIColor {
         if self.is_transparent() {
             return Ok(());
         }
+        let len = self.calc_inner_len();
         if f.sign_minus() {
-            write!(f, "{ANSI_BACKGROUND_ESCAPE}{}m", self.inner)
+            unsafe {
+                write!(f, "{ANSI_BACKGROUND_ESCAPE}{}m", std::str::from_utf8_unchecked(&self.inner[..len]))
+            }
         } else {
-            write!(f, "{ANSI_FOREGROUND_ESCAPE}{}m", self.inner)
+            unsafe {
+                write!(f, "{ANSI_FOREGROUND_ESCAPE}{}m", std::str::from_utf8_unchecked(&self.inner[..len]))
+            }
         }
     }
 }
